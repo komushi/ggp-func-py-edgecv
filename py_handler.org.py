0eba192ba28cf1edcda664bb8f0ea6dd14398c38
Lei Xu <komushi@gmail.com>
2024-05-27 20:34:07 +0900

    wip embedding

---
+++ py_handler.org.py
@@ -0,0 +1,147 @@
+import json
+import logging
+import time
+import datetime
+import sys
+import os
+from threading import Thread
+from multiprocessing.connection import Listener, Client
+
+import greengrasssdk
+
+import socket
+
+# Setup logging to stdout
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+# from libs import face_detector as fdm
+detector = None
+
+client = greengrasssdk.client("iot-data")
+
+def get_local_ip():
+    # Connect to an external host, in this case, Google's DNS server
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    return local_ip
+
+def motion(params):
+    try:
+        logger.info("motion start params:" + repr(params))
+
+        import motion_detector as mdm
+
+        global detector
+
+        detector = mdm.MotionDetector(params)
+
+        detector.start_detector()
+
+    except Exception as e:
+        logger.error("MotionDetector failure: " + repr(e))
+
+def face(params):
+    try:
+        logger.info("face start params:" + repr(params))
+
+        import face_detector as fdm
+
+        global detector
+
+        detector = fdm.FaceDetector(params)
+
+        detector.start_detector()
+        
+    except Exception as e:
+        logger.error("FaceDetector failure: " + repr(e))
+
+def recognition(params):
+    try:
+        logger.info("recognition start params:" + repr(params))
+
+        import face_recognition as fdm
+
+        global detector
+
+        detector = fdm.FaceRecognition(params)
+
+        detector.start_detector()
+        
+    except Exception as e:
+        logger.error("FaceRecognition failure: " + repr(e))
+
+def function_handler(event, context):
+    logger.info('function_handler event: ' + repr(event))
+
+    if 'method' in event and 'action' in event:
+        possibles = globals().copy()
+        possibles.update(locals())
+
+        method = possibles.get(event['method'])
+        action = event['action']
+
+        params = {}
+        
+        if 'rtsp_src' in event:
+            params['rtsp_src'] = event['rtsp_src']
+
+        if 'codec' in event:
+            params['codec'] = event['codec']
+
+        if 'framerate' in event:
+            params['framerate'] = event['framerate']
+
+        if 'face_file' in event:
+            params['face_file'] = event['face_file']
+
+        logger.info("function_handler params:" + repr(params))
+
+        if not method:
+            raise NotImplementedError("Method %s not implemented" % event['method'])
+            sys.exit(0)
+        else:
+            index = list(possibles.keys()).index(event['method'])
+            address = ('localhost', 6000 + index)
+
+            if action == 'start':
+                with Listener(address, authkey=b'secret password') as listener:                
+                    print('Listener Started')
+                    t = Thread(target=method, args=(params,))
+                    t.start()
+                    print('Prepare to accept')
+                    with listener.accept() as conn:
+                        print('connection accepted from', listener.last_accepted)
+                        conn.send_bytes(b'hello')
+                        global detector
+                        detector.stop_event.set()
+                        sys.exit()
+
+            elif action == 'stop':
+                with Client(address, authkey=b'secret password') as conn:
+                    print(conn.recv_bytes())
+                    sys.exit()
+
+    else:
+        data = {
+            "equipmentId": os.environ['AWS_IOT_THING_NAME'],
+            "equipmentName": os.environ['AWS_IOT_THING_NAME'],
+            "localIp": get_local_ip()
+        }
+        
+        print(json.dumps(data))
+
+        client.publish(
+            topic="gocheckin/scanner_detected",
+            payload=json.dumps(data)
+        )
+        sys.exit(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 6:
+        function_handler(event={"method": sys.argv[1], "action": sys.argv[2], "rtsp_src": sys.argv[3], "codec": sys.argv[4], "framerate": sys.argv[5]}, context=None)
+    elif len(sys.argv) == 7:
+        function_handler(event={"method": sys.argv[1], "action": sys.argv[2], "rtsp_src": sys.argv[3], "codec": sys.argv[4], "framerate": sys.argv[5], "face_file": sys.argv[6]}, context=None)
+    elif len(sys.argv) == 3:
+        function_handler(event={"method": sys.argv[1], "action": sys.argv[2]}, context=None)
--- py_handler.py
+++ py_handler.py
@@ -8,16 +8,22 @@ from threading import Thread
 from multiprocessing.connection import Listener, Client
 
 import greengrasssdk
-
 import socket
+
+import numpy as np
+import PIL.Image
+from insightface.app import FaceAnalysis
+
+import io
+import base64
+
 
 # Setup logging to stdout
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-# from libs import face_detector as fdm
-detector = None
 
+face_app = init_face_app()
 client = greengrasssdk.client("iot-data")
 
 def get_local_ip():
@@ -27,111 +33,62 @@ def get_local_ip():
         local_ip = s.getsockname()[0]
     return local_ip
 
-def motion(params):
-    try:
-        logger.info("motion start params:" + repr(params))
+def init_face_app():
+    app = FaceAnalysis(name='buffalo_sc', allowed_modules=['detection', 'recognition'], providers=['CUDAExecutionProvider', 'CPUExecutionProvider'], root='/etc/insightface')
+    app.prepare(ctx_id=0, det_size=(640, 640))#ctx_id=0 CPU
+    return app
 
-        import motion_detector as mdm
+def read_base64_face(base64_string):
+    # Step 1: Decode the Base64 string
+    image_data = base64.b64decode(base64_string)
+    
+    # Step 2: Open the image using PIL
+    image = PIL.Image.open(io.BytesIO(image_data)).convert("RGB")
+    
+    # Step 3: Convert the image to a NumPy array
+    image_array = np.array(image)
+    
+    # Step 4: Reorder the channels from RGB to BGR
+    image_bgr = image_array[:, :, [2, 1, 0]]  # RGB to BGR
+    
+    return image_bgr
 
-        global detector
-
-        detector = mdm.MotionDetector(params)
-
-        detector.start_detector()
-
-    except Exception as e:
-        logger.error("MotionDetector failure: " + repr(e))
-
-def face(params):
-    try:
-        logger.info("face start params:" + repr(params))
-
-        import face_detector as fdm
-
-        global detector
-
-        detector = fdm.FaceDetector(params)
-
-        detector.start_detector()
-        
-    except Exception as e:
-        logger.error("FaceDetector failure: " + repr(e))
-
-def recognition(params):
-    try:
-        logger.info("recognition start params:" + repr(params))
-
-        import face_recognition as fdm
-
-        global detector
-
-        detector = fdm.FaceRecognition(params)
-
-        detector.start_detector()
-        
-    except Exception as e:
-        logger.error("FaceRecognition failure: " + repr(e))
 
 def function_handler(event, context):
-    logger.info('function_handler event: ' + repr(event))
-
-    if 'method' in event and 'action' in event:
-        possibles = globals().copy()
-        possibles.update(locals())
-
-        method = possibles.get(event['method'])
-        action = event['action']
-
-        params = {}
+    logger.info('function_handler context: ' + repr(context))
+    if context.clientContext.Custom.subject == "gocheckin/req_face_embeddings":
         
-        if 'rtsp_src' in event:
-            params['rtsp_src'] = event['rtsp_src']
+        logger.info('function_handler req_face_embeddings event: ' + repr(event))
 
-        if 'codec' in event:
-            params['codec'] = event['codec']
+        image_bgr = read_base64_face(event.faceImgBase64)
 
-        if 'framerate' in event:
-            params['framerate'] = event['framerate']
+        reference_faces = face_app.get(image_bgr)
+        return 
 
-        if 'face_file' in event:
-            params['face_file'] = event['face_file']
+        data = {
+            "reservationCode": event.reservationCode,
+            "memberNo": event.memberNo,
+            "faceEmbedding": reference_faces[0].embedding
+        }
+        
+        print(json.dumps(data))
 
-        logger.info("function_handler params:" + repr(params))
+        client.publish(
+            topic="gocheckin/res_face_embeddings",
+            payload=json.dumps(data)
+        )
+        sys.exit(0)
 
-        if not method:
-            raise NotImplementedError("Method %s not implemented" % event['method'])
-            sys.exit(0)
-        else:
-            index = list(possibles.keys()).index(event['method'])
-            address = ('localhost', 6000 + index)
+    elif context.clientContext.Custom.subject == f"gocheckin/{AWS_IOT_THING_NAME}/init_scanner":
+        logger.info('function_handler init_scanner')
 
-            if action == 'start':
-                with Listener(address, authkey=b'secret password') as listener:                
-                    print('Listener Started')
-                    t = Thread(target=method, args=(params,))
-                    t.start()
-                    print('Prepare to accept')
-                    with listener.accept() as conn:
-                        print('connection accepted from', listener.last_accepted)
-                        conn.send_bytes(b'hello')
-                        global detector
-                        detector.stop_event.set()
-                        sys.exit()
-
-            elif action == 'stop':
-                with Client(address, authkey=b'secret password') as conn:
-                    print(conn.recv_bytes())
-                    sys.exit()
-
-    else:
         data = {
             "equipmentId": os.environ['AWS_IOT_THING_NAME'],
             "equipmentName": os.environ['AWS_IOT_THING_NAME'],
             "localIp": get_local_ip()
         }
         
-        print(json.dumps(data))
-
+        # print(json.dumps(data))
         client.publish(
             topic="gocheckin/scanner_detected",
             payload=json.dumps(data)
