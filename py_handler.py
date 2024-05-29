fa8238098be8bcbe70b839135865ff74d2718614
Lei Xu <komushi@gmail.com>
2024-05-29 09:32:02 +0900

    embedding req http localhost

--- py_handler.py
+++ py_handler.py
@@ -81,18 +81,19 @@ def start_http_server():
 
                 reference_faces = face_app.get(image_bgr)
 
-                data = {
-                    "reservationCode": event['reservationCode'],
-                    "memberNo": event['memberNo'],
-                    "faceEmbedding": reference_faces[0].embedding.tolist()
-                }
-        
-                iotClient.publish(
-                    topic="gocheckin/res_face_embeddings",
-                    payload=json.dumps(data)
-                )
+                event['faceEmbedding'] = reference_faces[0].embedding.tolist()
 
-                # Example response
+                # data = {
+                #     "reservationCode": event['reservationCode'],
+                #     "memberNo": event['memberNo'],
+                #     "faceEmbedding": reference_faces[0].embedding.tolist()
+                # }
+        
+                # iotClient.publish(
+                #     topic="gocheckin/res_face_embeddings",
+                #     payload=json.dumps(data)
+                # )
+
                 bbox = reference_faces[0].bbox.astype(np.int).flatten()
                 cropped_face = org_image.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
 
@@ -102,6 +103,7 @@ def start_http_server():
                 cropped_face_bytes = buffered.getvalue()
 
                 event['faceImgBase64'] = base64.b64encode(cropped_face_bytes).decode('utf-8')
+                
 
                 response = {'message': 'Recognition completed', 'data': event}
 
